fdd53fe4127f285b052028e561ae3e64e9980ae1
Frank Braungardt <2-frankyb@users.noreply.gitlab.example.com>
2021-01-17 09:22:57 +0100

    removed hdd-filling logging

--- script.services.shutdownsteamcontrollers/resources/lib/shutdownsteamcontrollers.sh
+++ script.services.shutdownsteamcontrollers/resources/lib/shutdownsteamcontrollers.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo called $@ >> /tmp/ssc.log
 if [ "${1}" = "start" ]; then
   while true; do
     sleep 2
@@ -9,6 +8,4 @@ controllers=1
 while [ ${controllers} -gt 0 ]; do
   controllers=`echo 'Turnoff.' | /usr/bin/nc -w1 -U ~/.config/scc/daemon.socket | grep "Controller Count: " | awk 'END{print $3}'`
   echo Another ${controllers} present
-  echo Another ${controllers} present >> /tmp/ssc.log
 done
-echo done. >> /tmp/ssc.log
